36582edde6723ec78cdd452ef56d8f9307b077fd
Ray Duran <baycool0422@gmail.com>
2019-10-21 11:17:03 -0700

    There still is a bug with the BuildSamplesForMC.m, where the Raman data for the second fish is not of one type. The improvement is that we now do the construction of the array in a function so the code is a bit more compact.

--- BuildSamplesForMC.m
+++ BuildSamplesForMC.m
@@ -4,21 +4,25 @@
 % University of North Dakota 
 % EE 999 ; Professor Tavakolian
 %%---------------------------------------------------------
-<<<<<<< HEAD
 
-min      = 250;
+
+min           = 250;
 modeArray     = table2cell(tbl_raman);
+ArrayForMC   = {};
 
-ArrayForMC = {};
 [assembledArrayForMC] = ConstructArrayForMC(modeArray,ArrayForMC,min,...
                                             blueTilapiaArray,blueTilapiaCount);
                                         
 ArrayForMC = assembledArrayForMC;
+
+debug = 0;
  
 [assembledArrayForMC] = ConstructArrayForMC(modeArray,ArrayForMC,min,...
                                             malabarSnapperArray,malabarSnapperCount);
                                         
 ArrayForMC = assembledArrayForMC;
+
+debug = 0;
  
 [assembledArrayForMC] = ConstructArrayForMC(modeArray,ArrayForMC,min,...
                                             redSnapperArray,redSnapperCount);
@@ -38,123 +42,29 @@ ArrayForMC = assembledArrayForMC;
 [assembledArrayForMC] = ConstructArrayForMC(modeArray,ArrayForMC,min,...
                                             whiteBassArray,whiteBassCount);
 
-%% ??? FInish this function
 
-function [assembledArrayForMC] = BuildSamplesForMC(tbl_data,ArrayForMC,min,...
+function [assembledArrayForMC] = ConstructArrayForMC(modeArray,ArrayForMC,min,...
                                   fishTypeArray,fishTypeCount)
 
+Test     = modeArray;
+RandomIndex = randsample(fishTypeCount,min);
+RandomIndex = RandomIndex';
 
-Test     = table2cell(tbl_data);
-
-
-tic
-% Build MC data for blue Tilapia
-blueTilapiaRandomIndex = randsample(fishTypeCount,min);
-=======
-%{
-function [assembledArrayForMC] = BuildSamplesForMC(tbl_data,min,...
-                                  blueTilapiaArray,blueTilapiaCount,...
-                                  malabarSnapperArray,malabarSnapperCount,...
-                                  redSnapperArray,redSnapperCount,...
-                                  summerFlounderArray,summerFlounderCount,...
-                                  vermillionSnapperArray,vermillionSnapperCount,...
-                                  whiteBassArray,whiteBassCount)
-
-%} 
-min = 250;
-Test     = table2cell(tbl_raman);
-ArrayForMC = {};
-
-tic
-% Build MC data for blue Tilapia
-blueTilapiaRandomIndex = randsample(blueTilapiaCount,min);
->>>>>>> 5731b35796920791182586afbdbf850083ed5ea5
-blueTilapiaRandomIndex = blueTilapiaRandomIndex';
 
 for i = 1: min
-    index = blueTilapiaRandomIndex(i);
-    blueTilapiaTrueIndex = blueTilapiaArray(index); % Simple Value class
-    blueTilapiaTrueNumIndex = blueTilapiaTrueIndex.Value;
-    cellrow       = Test(blueTilapiaTrueNumIndex,:);
+    index = RandomIndex(i);
+    fishTrueIndex = fishTypeArray(index); % Simple Value class
+    fishTrueNumIndex = fishTrueIndex.Value;
+    cellrow       = Test(fishTrueNumIndex,:);
     ArrayForMC    = [ ArrayForMC; cellrow];
     debug = 0;
 end
 
 debug = 0;
-
-% Build MC data for Malabar Snapper
-malabarSnapperRandomIndex = randsample(malabarSnapperCount,min);
-malabarSnapperRandomIndex = malabarSnapperRandomIndex';
-
-for i = 1: min
-    index = malabarSnapperRandomIndex(i);
-    malabarSnapperTrueIndex = malabarSnapperArray(index); % Simple Value class
-    malabarSnapperTrueNumIndex = malabarSnapperTrueIndex.Value;
-    cellrow       = Test(malabarSnapperTrueNumIndex,:);
-    ArrayForMC    = [ ArrayForMC; cellrow];
-    debug = 0;
+assembledArrayForMC = ArrayForMC;
 end
 
-debug = 0;
-
-% Build MC data for Red Snapper
-redSnapperRandomIndex = randsample(redSnapperCount,min);
-redSnapperRandomIndex = redSnapperRandomIndex';
-
-for i = 1: min
-    index = redSnapperRandomIndex(i);
-    redSnapperTrueIndex = redSnapperArray(index); % Simple Value class
-    redSnapperTrueNumIndex = redSnapperTrueIndex.Value;
-    cellrow       = Test(redSnapperTrueNumIndex,:);
-    ArrayForMC    = [ ArrayForMC; cellrow];
-    debug = 0;
-end
-
-debug = 0;
-
-% Build MC data for Summer Flounder
-summerFlounderRandomIndex = randsample(summerFlounderCount,min);
-summerFlounderRandomIndex = summerFlounderRandomIndex';
-
-for i = 1: min
-    index = summerFlounderRandomIndex(i);
-    summerFlounderTrueIndex = summerFlounderArray(index); % Simple Value class
-    summerFlounderTrueNumIndex = summerFlounderTrueIndex.Value;
-    cellrow       = Test(summerFlounderTrueNumIndex,:);
-    ArrayForMC    = [ ArrayForMC; cellrow];
-    debug = 0;
-end
-%toc
-debug = 0;
-
-% Build MC data for Vermillion Snapper
-vermillionSnapperRandomIndex = randsample(vermillionSnapperCount,min);
-vermillionSnapperRandomIndex = vermillionSnapperRandomIndex';
-
-for i = 1: min
-    index = vermillionSnapperRandomIndex(i);
-    vermillionSnapperTrueIndex = vermillionSnapperArray(index); % Simple Value class
-    vermillionSnapperTrueNumIndex = vermillionSnapperTrueIndex.Value;
-    cellrow       = Test(vermillionSnapperTrueNumIndex,:);
-    ArrayForMC    = [ ArrayForMC; cellrow];
-    debug = 0;
-end
-%toc
-debug = 0;
-
-% Build MC data for White Bass
-whiteBassRandomIndex = randsample(whiteBassCount,min);
-whiteBassRandomIndex = whiteBassRandomIndex';
-
-for i = 1: min
-    index = whiteBassRandomIndex(i);
-    whiteBassTrueIndex = whiteBassArray(index); % Simple Value class
-    whiteBassTrueNumIndex = whiteBassTrueIndex.Value;
-    cellrow       = Test(whiteBassTrueNumIndex,:);
-    ArrayForMC    = [ ArrayForMC; cellrow];
-    debug = 0;
-end
-toc
-debug = 0;
 
 
+
+%}
